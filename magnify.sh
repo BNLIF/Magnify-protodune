32b8391cbdd25b5656fda8924983a186af093eab
Wenqiang Gu <goowenq@gmail.com>
2019-02-12 22:48:55 -0500

    interactive installer for singularity/wct

--- magnify.sh
+++ magnify.sh
@@ -8,7 +8,7 @@ if [[ "$rootfile" =~ :// ]] ; then
 else
     rootfile=$(readlink -f "$rootfile")
 fi
-threshold="${1:-18}"
+threshold="${1:-30}"
 frame="${2:-decon}"
 startdir=$(pwd)
 rebin="${3:-1}"
